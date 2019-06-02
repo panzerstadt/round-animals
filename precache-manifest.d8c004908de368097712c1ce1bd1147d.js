60f819d0deaadf6d02cea54eebc5a283ed6069c7
panzerstadt <tliqun@gmail.com>
2019-06-02 14:40:47 +0900

    Updates

---
+++ precache-manifest.d8c004908de368097712c1ce1bd1147d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd205aaeed57bcd7e3a8f8e23912bc0c",
+    "url": "/round-animals/index.html"
+  },
+  {
+    "revision": "9a880fef3d2adefe03dd",
+    "url": "/round-animals/static/css/main.c314b030.chunk.css"
+  },
+  {
+    "revision": "3a1106cc801a4a4aa26d",
+    "url": "/round-animals/static/js/2.99976094.chunk.js"
+  },
+  {
+    "revision": "9a880fef3d2adefe03dd",
+    "url": "/round-animals/static/js/main.818f5d4a.chunk.js"
+  },
+  {
+    "revision": "7acf641a9257c8efe03c",
+    "url": "/round-animals/static/js/runtime~main.4ec6ccd5.js"
+  },
+  {
+    "revision": "de75430885d400462cb53e9700bcab9c",
+    "url": "/round-animals/static/media/chick_0004_body.de754308.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/round-animals/static/media/logo.5d5d9eef.svg"
+  }
+]);
